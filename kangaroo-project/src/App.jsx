13f0003a5a3e4f685af72e08435668afb2b93764
benv12 <151891474+benv12@users.noreply.github.com>
2024-09-10 13:07:00 -0500

    made the search bar width by a percentage so it isnt determined by pixels. also added a page folder under components that will have the pages for the website

--- kangaroo-project/src/App.jsx
+++ kangaroo-project/src/App.jsx
@@ -1,10 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Page/Home'
 
 const App = () => {
   return (
-    <div><Navbar/></div>
+    <div>
+      <Navbar/>
+      <Home/>
+    </div>
+
   )
 }
 
--- kangaroo-project/src/Components/Navbar/Navbar.jsx
+++ kangaroo-project/src/Components/Navbar/Navbar.jsx
@@ -4,23 +4,46 @@ import './Navbar.css'
 import searchIcon from '../../assets/search-b.png'
 
 const Navbar = () => {
-  return (
-    <header className="header">
-        <a href="" className="logo">Kangaroo</a>
-        
-        <div className="search-Box">
-        <input type='text' placeholder='Search Roo'/>
-        <img src={searchIcon}/>
-        
-        </div>
+  const isAdmin = true; 
 
-        <nav className="navbar">
-            <a href="/">Create</a>
-            <a href="/">Notifications</a>
-            <a href="/">Log In</a>
-        </nav>
-    </header>
-  )
+  if(isAdmin){
+    return (
+      <header className="header">
+          <a href="" className="logo">Kangaroo</a>
+          
+          <div className="search-Box">
+          <input type='text' placeholder='Search Roo'/>
+          <img src={searchIcon}/>
+          
+          </div>
+  
+          <nav className="navbar">
+              <a href="/">Create</a>
+              <a href="/">Notifications</a>
+              <a href="/">Admin</a>
+              <a href="/">Log In</a>
+          </nav>
+      </header>
+    )}else{
+      return (
+        <header className="header">
+            <a href="" className="logo">Kangaroo</a>
+            
+            <div className="search-Box">
+            <input type='text' placeholder='Search Roo'/>
+            <img src={searchIcon}/>
+            
+            </div>
+    
+            <nav className="navbar">
+                <a href="/">Create</a>
+                <a href="/">Notifications</a>
+                <a href="/">Log In</a>
+            </nav>
+        </header>
+      )
+    }
+  
 }
 
 export default Navbar

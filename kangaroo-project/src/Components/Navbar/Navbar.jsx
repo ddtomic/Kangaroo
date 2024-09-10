0693648641d53f08a37970068a6c8c442a4d46ca
benv12 <151891474+benv12@users.noreply.github.com>
2024-09-09 23:08:28 -0500

    few things: i used vite which is just a js tool. created a basic nav bar. color is just green because i like green we can change it whatever later on. Created a component folder with a navbar folder and inside that is where the navbar.jsx and navbar.css is

---
+++ kangaroo-project/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Navbar.css'
+import searchIcon from '../../assets/search-b.png'
+
+const Navbar = () => {
+  return (
+    <header className="header">
+        <a href="" className="logo">Kangaroo</a>
+        
+        <div className="search-Box">
+        <input type='text' placeholder='Search Roo'/>
+        <img src={searchIcon}/>
+        
+        </div>
+
+        <nav className="navbar">
+            <a href="/">Create</a>
+            <a href="/">Notifications</a>
+            <a href="/">Log In</a>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar

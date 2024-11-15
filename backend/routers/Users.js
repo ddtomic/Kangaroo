0b95ea999637fdb27f757de0565077462d11cb5b
Ayham Zahabi <wazabe45@gmail.com>
2024-11-15 16:22:34 -0600

    ec2 backend test

--- backend/routers/Users.js
+++ backend/routers/Users.js
@@ -3,22 +3,26 @@ const router = express.Router();
 const { Users } = require("../models");
 const bcrypt = require("bcrypt");
 
-router.post("/signup", async (req, res) => {
-  const { username, email, password, bio } = req.body;
-  bcrypt.hash(password, 10).then((hash) => {
-    Users.create({
-      username: username,
-      email: email,
-      password: hash,
-      bio: bio,
+router.post("/", (req, res) => {
+  try {
+    const { username, email, password, bio } = req.body;
+    bcrypt.hash(password, 10).then((hash) => {
+      Users.create({
+        username: username,
+        email: email,
+        password: hash,
+        bio: bio,
+      });
+      res.json("Success!");
     });
-    res.json("Success!");
-  });
+  } catch (error) {
+    res.status(500).send("Can't create user");
+  }
 });
 
 router.get("/users", async (req, res) => {
   try {
-    const users = await User.findAll(); // Sequelize method to fetch all records
+    const users = await Users.findAll(); // Sequelize method to fetch all records
     res.json(users);
   } catch (error) {
     res.status(500).send("Error fetching users");

faf03f51c9fb6e4ee4386b8529d4dc458b2cba98
benv12 <151891474+benv12@users.noreply.github.com>
2024-11-20 15:32:21 -0600

    DropDown redesign

--- frontend/src/Components/Dropdown.jsx
+++ frontend/src/Components/Dropdown.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 import '../CSS/Components/Dropdown.css'
 import CommentNotification from '../Props/CommentNotification'
 import LikeNotification from '../Props/LikeNotification'
+import close from '../assets/images/close.png'
+
 
 const Dropdown = () => {
+  const [ DropDown, setDropDown ] = useState(false);
   return (
     <div className='notify'>
+      
         <div className="dropdown">
+          <div className="close-dropdown">
+         <a onClick={() => setDropDown((prev) => !prev)}><img src={close} alt='close-image'></img></a>
+          </div>
             <ul className='notify-list'>
                <CommentNotification name='Ben' date='11/11/2024' user='dejan' ></CommentNotification>
                <CommentNotification name='Josue' date='11/11/2024' user='Ben' ></CommentNotification>
@@ -15,6 +23,7 @@ const Dropdown = () => {
 
             </ul>
         </div>
+        
     </div>
   )
 }
